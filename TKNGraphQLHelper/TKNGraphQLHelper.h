87f36ddc58db326cf25ac939218b3a37dcdb3e77
Alper Sevindik <alper.sevindik@pirimedya.com>
2019-11-25 18:02:37 +0300

    Initial Commit

---
+++ TKNGraphQLHelper/TKNGraphQLHelper.h
@@ -0,0 +1,19 @@
+//
+//  TKNGraphQLHelper.h
+//  TKNGraphQLHelper
+//
+//  Created by Alper Sevindik on 25.11.2019.
+//  Copyright © 2019 Alper Sevindik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TKNGraphQLHelper.
+FOUNDATION_EXPORT double TKNGraphQLHelperVersionNumber;
+
+//! Project version string for TKNGraphQLHelper.
+FOUNDATION_EXPORT const unsigned char TKNGraphQLHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKNGraphQLHelper/PublicHeader.h>
+
+
